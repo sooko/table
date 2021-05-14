d5d3073b638bc675915c461d73d8522cc978ce36
sooko <hansmotor89@gmail.com>
2021-05-15 06:10:15 +0700

    first package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+  name='table',
+  version='0.1.0',
+  author='burhan',
+  author_email='hansmotor89@gmail.com',
+  packages=['table'],
+  url='https://github.com/sooko/table.git',
+  license='LICENSE.txt',
+  description='An awesome package that does something',
+  long_description="open('README.txt').read()",
+  
+) 
